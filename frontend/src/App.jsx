7134383767a2fe64252c7ccd82859c877b161547
Sadmanul Hoque <sadmanulhoque21@gmail.com>
2026-08-13 00:42:49 +0600

    feat: mastering templte epi2 30 min

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from './pages/Menu';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Layout from "./layouts/Layout";
 export default function App() {
-  return (
-    <>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
-  )
+    return (
+
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/menu" element={<Menu />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/register" element={<Register />}></Route>
+            </Routes>
+        </Layout>
+
+    );
 }
-
-
---
+++ frontend/src/layouts/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from "../layouts/Header"
+import Footer from "../layouts/Footer"
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <Header />
+                {children}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
